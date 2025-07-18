cbd0cb38e130bf2eff5a2b29ec37980e0cff50d4
Kishore Kumar A V <kishore.kumar@techolution.com>
2025-07-18 21:01:17 +0530

    fixed violations of core standards

--- remove_nth_node_from_end.py
+++ remove_nth_node_from_end.py
@@ -4,19 +4,21 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        d = ListNode(0, head)
-        slow = fast = d
+    def removeNthFromEnd(self, head: Optional[ListNode], n_from_end: int) -> Optional[ListNode]:
+        print("starting process")
+        dummy_head = ListNode(0, head)
+        slow_pointer = fast_pointer = d
 
-        for _ in range(n+1):
-            fast = fast.next
+        for i in range(n+1):
+            print("current count - ", i)
+            fast_pointer = fast_pointer.next
         
-        while fast:
-            slow = slow.next
-            fast = fast.next
+        while fast_pointer:
+            slow_pointer = slow_pointer.next
+            fast_pointer = fast_pointer.next
 
-        slow.next = slow.next.next
+        slow_pointer.next = slow_pointer.next.next
         # 0->1->3->4->5
-        return d.next
+        return dummy_head.next
 
         
